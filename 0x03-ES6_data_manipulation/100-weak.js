e2219d2e5837b6a02cee0df45c013f7034da0564
amorezitha <amorezitha@gmail.com>
2024-04-30 03:27:12 -0700

    updated

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,10 @@
-export default function updateUniqueItems(olMap) {
-	if (!(olMap instanceof Map)) throw Error('Cannot process');
-	olMap.forEach((value, key) => {
-		if (value === 1) olMap.set(key, 100);
-	});
+export const weakMap = new WeakMap();
+let count = 1;
+export function queryAPI(endPoint) {
+	weakMap.set(endPoint, count);
+	count += 1;
+	const queryCount = weakMap.get(endPoint);
+	if (queryCount >= 5) {
+		throw new Error('Endpoint load is high');
+	}
 }
